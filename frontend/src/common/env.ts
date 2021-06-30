35a956f6802ea5f303cc7c08aaf87f1f63ab39fe
riptakagi <smatchjp@gmail.com>
2021-06-30 18:27:44 +0900

    fix: login path w/ env api path

--- frontend/src/common/env.ts
+++ frontend/src/common/env.ts
@@ -3,6 +3,6 @@ import { Env } from "../typings/struct";
 export default {
     HOST: process.env.HOST ? process.env.HOST : '0.0.0.0',
     PORT: process.env.PORT ? process.env.PORT : '3000',
-    BACKEND_BASE_URI: process.env.BACKEND_URI ? process.env.BACKEND_URI : "http://localhost:3001/api/",
+    BACKEND_BASE_URI: process.env.BACKEND_URI ? process.env.BACKEND_URI : "https://dispotify.iamtakagi.net/api",
 } as Env
   
