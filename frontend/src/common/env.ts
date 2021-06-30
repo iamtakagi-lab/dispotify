cbad240f141349d5c61782f8d5852449c3295914
riptakagi <smatchjp@gmail.com>
2021-07-01 02:50:15 +0900

    revert: backend base url

--- frontend/src/common/env.ts
+++ frontend/src/common/env.ts
@@ -3,5 +3,5 @@ import { Env } from "../typings/struct";
 export default {
     HOST: process.env.HOST ? process.env.HOST : '0.0.0.0',
     PORT: process.env.PORT ? process.env.PORT : '3000',
-    BACKEND_BASE_URL: process.env.BACKEND_BASE_URL ? process.env.BACKEND_BASE_URL : "http://localhost:3001/api/"
+    BACKEND_BASE_URL: process.env.BACKEND_BASE_URL ? process.env.BACKEND_BASE_URL : "https://dispotify.iamtakagi.net/api"
 } as Env
