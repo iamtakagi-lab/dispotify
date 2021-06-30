7887e1edfb6b52680baaa40f0c862c774fc1fff8
riptakagi <smatchjp@gmail.com>
2021-06-30 19:45:41 +0900

    fix: spacing

--- frontend/src/components/global/Header.tsx
+++ frontend/src/components/global/Header.tsx
@@ -22,7 +22,7 @@ export const Header: React.VFC<{}> = () => {
       <div className="desc w-full my-6 space-y-1">
         <a href="" className="text-xl">Dispotify</a>
         <p className="text-sm">Spotify to Discord notifier / Spotifyで再生中の音楽をDiscordに通知</p>
-        <p className="text-sm">現在: {status ? status.usersCount : 0} 人のユーザーが利用しています</p>
+        <p className="text-sm">現在{status ? status.usersCount : 0}人のユーザーが利用しています</p>
       </div>
 
       <div className="mt-5">
